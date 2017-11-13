5217c672c2cf3f8bc0b2b49f6e447917294b94e9
Sadi <shah.kutub1@gmail.com>
2017-11-13 21:26:09 +0600

    2nd commit

--- app/src/main/java/com/sadi/offerian/LandingActivity.java
+++ app/src/main/java/com/sadi/offerian/LandingActivity.java
@@ -1,8 +1,10 @@
 package com.sadi.offerian;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -15,13 +17,29 @@ public class LandingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing_page);
 
         con  = this;
         initialization();
     }
 
     private void initialization() {
+        tvLogin = (TextView)findViewById(R.id.tvLogin);
+        tvSignUp = (TextView)findViewById(R.id.tvLogin);
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(con,SignUpActivity.class));
+            }
+        });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(con,SignUpActivity.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/sadi/offerian/SignUpActivity.java
+++ app/src/main/java/com/sadi/offerian/SignUpActivity.java
@@ -1,8 +1,10 @@
 package com.sadi.offerian;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -11,7 +13,7 @@ import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
     Context con;
-    private TextView tvSignUp,tvLogin;
+    private TextView tvSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,13 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void initialization() {
+        tvSubmit = (TextView)findViewById(R.id.tvSubmit);
 
+        tvSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(con,MainActivity.class));
+            }
+        });
     }
 }
